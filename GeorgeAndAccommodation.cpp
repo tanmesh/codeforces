b3af7afc95e19fa82b0af905bbce0b55f6a92359
Tanmesh Mishra <tanmeshnm@gmail.com>
2016-12-21 18:12:22 +0530

    GeorgeAndAccommodation 21Dec16

---
+++ GeorgeAndAccommodation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n,cnt=0;
+	cin >> n;
+	int p[n],q[n];
+	for(int i=0;i<n;++i){
+		cin >> p[i] >> q[i]; 
+		if(q[i]-p[i] >= 2){
+			++cnt;
+		}
+	}
+	cout << cnt << endl;
+
+}
